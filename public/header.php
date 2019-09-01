164165699eaddd6638dc4e687d7e9c25aa13f531
Byron <byronortizrojas0998@gmail.com>
2019-08-31 18:03:33 -0600

    Commit 2

--- public/header.php
+++ public/header.php
@@ -1,52 +1,36 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 
 <head>
+
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Proyecto Lenguajes</title>
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="public/css/estilos.css"/>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-            crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <title>Audio World</title>
 
-    <link href='http://fonts.googleapis.com/css?family=Tangerine:400,700' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Lobster+Two:400,400italic' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-          integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <script type="text/javascript" src="public/js/jquery-3.3.1.js"></script>
-    <script type="text/javascript" src="public/js/script.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-
-    <link rel="stylesheet" type="text/css" href="public/css/tabla1.css">
-    <script type="text/javascript" charset="utf8" src="public/js/table.js"></script>
+    <!-- Custom styles for this template -->
+    <link href="public/css/business-frontpage.css" rel="stylesheet">
 
 </head>
 
-<body id="paginaPrincipal" data-spy="scroll" data-target=".navbar" data-offset="60">
-<div class="contenedor">
+<body>
 
 
-    <div id="inicio" class="row jumbotron text-center">
-        <div class="col-sm-2">
-        </div>
-        <div class="col-sm-3">
-            <img id="Logo" alt="Logo" src="public/imgs/tienda.png"
-                                   width="400" height="400"></div>
-        <div class="col-sm-7">
-            <h2 id="titulo">Compra en linea</h2>
-            <h2>
-                <br><br><br>
-                Encuentra los mejores productos en la comodidad de tu casa
-            </h2>
 
+<!-- Header -->
+<header class="bg-primary py-5 mb-5">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-lg-12">
+                <h1 class="display-4 text-white mt-5 mb-2">Los mejores productos en audio profesional!</h1>
+                <p class="lead mb-5 text-white-50">Tenemos todo lo necesario para que disfrutes de la mejor experiencia en audio, con productos de alta
+                    calidad, de las mejores marcas y el mejor precio del mercado.</p>
+            </div>
         </div>
     </div>
-
-    <div id="principal" class="container-fluid">
+</header>
